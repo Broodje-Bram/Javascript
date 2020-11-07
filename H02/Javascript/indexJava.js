b703dd38eb194a12bf668d424ba76b819278c81c
Bramg <bramgoudsmid@hotmail.com>
2020-11-07 13:11:48 +0100

    Opdracht af

---
+++ H02/Javascript/indexJava.js
@@ -0,0 +1,13 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+
+document.getElementById("aap1").src = "Img/aap1.jpg";
+document.getElementById("aap2").src = "Img/aap2.jpg";
+
+document.getElementById("blok1").style.backgroundColor = "cyan";
+document.getElementById("blok2").style.backgroundColor = "green";
+document.getElementById("blok3").style.backgroundColor = "yellow";
+document.getElementById("blok4").style.backgroundColor = "red";
+
+document.getElementById("blok2").style.marginTop = "30px";
+document.getElementById("blok3").style.marginTop = "40px";
