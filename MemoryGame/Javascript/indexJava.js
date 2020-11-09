1c9e90ffb21dd63d47d1189a8b6798586d5a85db
Bramg <bramgoudsmid@hotmail.com>
2020-11-09 23:59:15 +0100

    versie 3.0

--- MemoryGame/Javascript/indexJava.js
+++ MemoryGame/Javascript/indexJava.js
@@ -1,8 +1,7 @@
-
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
-let lockBoard = false;
+let lockBoard = false
 let firstCard, secondCard;
 
 function flipCard() {
@@ -10,27 +9,26 @@ function flipCard() {
     if (this === firstCard) return;
 
     this.classList.add('flip');
-
+    
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
 
         return;
     }
-
     secondCard = this;
-    checkForMatch();
+
+    checkingformatch();
 }
 
-function checkForMatch() {
+function checkingformatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
     isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+    firstCard.removeEventListener('click', flipCard())
+    secondCard.removeEventListener('click', flipCard())
 
     resetBoard();
 }
@@ -47,13 +45,15 @@ function unflipCards() {
 }
 
 function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
 }
 
-(function shuffle() {
+(function schudden() {
     cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
+        let randomPos = Math.floor(math.random() * 12);
         card.style.order = randomPos;
     });
 })();
