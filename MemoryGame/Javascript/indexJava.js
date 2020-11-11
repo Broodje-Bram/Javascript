0e0f8a12cfcb7a3a4b33d28951da75bf578ef7d4
Bramg <bramgoudsmid@hotmail.com>
2020-11-11 01:42:36 +0100

    game scoreboard en beurten werken

--- MemoryGame/Javascript/indexJava.js
+++ MemoryGame/Javascript/indexJava.js
@@ -3,6 +3,18 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let player1 = document.getElementById('player1points');
+let player1pointscore = 0;
+let player2 = document.getElementById('player2points');
+let player2pointscore = 0;
+
+var turnCount = 0;
+var winner = null;
+var playerbeurt = null;
+var thewinner = document.getElementById("thewinner");
+var debeurt = document.getElementById("debeurt");
+thewinner.style.visibility = 'hidden'
+debeurt.style.visibility = 'visible'
 
 function flipCard() {
     if (lockBoard) return;
@@ -22,7 +34,12 @@ function flipCard() {
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch
+    if ( isMatch = firstCard.dataset.framework === secondCard.dataset.framework) {
+        playerscore();
+    }
+
+
 
     isMatch ? disableCards() : unflipCards();
 }
@@ -43,6 +60,8 @@ function unflipCards() {
 
         resetBoard();
     }, 1500);
+
+    beurt();
 }
 
 function resetBoard() {
@@ -56,5 +75,56 @@ function resetBoard() {
         card.style.order = randomPos;
     });
 })();
+
+function beurt() {
+    turnCount++
+    if (turnCount % 2) {
+        playerbeurt = "Player2 is aan de beurt";
+    }
+    else {
+        playerbeurt = "Player1 is aan de beurt";
+    }
+    debeurt.innerHTML = playerbeurt;
+    console.log(turnCount + "turncount");
+}
+
+function playerscore() {
+    if (turnCount % 2) {
+        player2pointscore +=1;
+        player2.innerHTML = player2pointscore;
+
+        console.log(player2pointscore + "player 2")
+    }
+    else  {
+        player1pointscore +=1;
+        player1.innerHTML = player1pointscore;
+
+        console.log(player1pointscore)
+    }
+    endgame();
+}
+
+function endgame() {
+    if (player1pointscore + player2pointscore < 10) {
+
+    } else {
+        console.log("ENDGAME IS NEUR");
+        whowon();
+    }
+
+}
+
+function whowon() {
+    if (player1pointscore > player2pointscore) {
+        winner = "Player1 is de Winnaar";
+    }
+    else {
+        winner = "Player2 is de Winnaar";
+    }
+    console.log(winner);
+    thewinner.innerHTML = winner;
+    thewinner.style.visibility = 'visible'
+    debeurt.style.visibility = 'hidden'
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
